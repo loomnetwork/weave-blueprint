1e4243a0dfce9dc2ecd8fa807933a4cad33b25a9
extrodox <myhemen@gmail.com>
2018-06-02 01:00:50 +0530

    Rewite main.go to use go-loom package for contract call managements.

--- src/cli/main.go
+++ src/cli/main.go
@@ -9,28 +9,18 @@ import (
 	"os"
 	"types"
 
-	"github.com/loomnetwork/go-loom/auth"
-	"github.com/loomnetwork/go-loom/client"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/ed25519"
+	"github.com/loomnetwork/go-loom/cli"
 )
 
 type MessageData struct {
 	Value int
 }
 
-func main() {
-	var writeURI, readURI, contractName, chainID, privFile, user string
-	rootCmd := &cobra.Command{
-		Use:   "blueprint",
-		Short: "blueprint example",
-	}
-	rootCmd.PersistentFlags().StringVarP(&writeURI, "write", "w", "http://localhost:46658/rpc", "URI for sending txs")
-	rootCmd.PersistentFlags().StringVarP(&readURI, "read", "r", "http://localhost:46658/query", "URI for quering app state")
-	rootCmd.PersistentFlags().StringVarP(&contractName, "contract", "", "BluePrint", "contract address")
-	rootCmd.PersistentFlags().StringVarP(&chainID, "chain", "", "default", "chain ID")
-
+func getKeygenCmd() (*cobra.Command) {
+	var privFile string
 	keygenCmd := &cobra.Command{
 		Use:           "genkey",
 		Short:         "generate a public and private key pair",
@@ -50,6 +40,26 @@ func main() {
 		},
 	}
 	keygenCmd.Flags().StringVarP(&privFile, "key", "k", "key", "private key file")
+	return keygenCmd
+}
+
+func main() {
+	var user string
+	defaultContract := "BluePrint"
+
+	rootCmd := &cobra.Command{
+		Use:   "blueprint",
+		Short: "Blueprint",
+	}
+
+	keygenCmd := getKeygenCmd()
+
+	callCmd := cli.ContractCallCommand()
+	rootCmd.AddCommand(callCmd)
+
+
+
+	rootCmd.AddCommand(keygenCmd)
 
 	createAccCmd := &cobra.Command{
 		Use:           "create-acct",
@@ -57,29 +67,14 @@ func main() {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			privKey, err := ioutil.ReadFile(privFile)
-			if err != nil {
-				return err
-			}
-
-			privKey, err = base64.StdEncoding.DecodeString(string(privKey))
-			if err != nil {
-				return errors.Wrapf(err, "Cannot read priv file: %s", privFile)
-			}
-
-			signer := auth.NewEd25519Signer(privKey)
 			payload := &types.BluePrintCreateAccountTx{
 				Version: 1,
 				Owner:   user,
 				Data:    []byte(user),
 			}
 
-			contract, err := newContract(chainID, writeURI, readURI, contractName)
+			err := cli.CallContract(defaultContract, "CreateAccount", payload, nil)
 			if err != nil {
-				return err
-			}
-
-			if _, err := contract.Call("CreateAccount", payload, signer, nil); err != nil {
 				return errors.Wrap(err, "contract call error")
 			}
 			fmt.Printf("user %s created successfully!\n", user)
@@ -87,7 +82,6 @@ func main() {
 		},
 	}
 	createAccCmd.Flags().StringVarP(&user, "user", "u", "loom", "user")
-	createAccCmd.Flags().StringVarP(&privFile, "key", "k", "key", "private key file")
 
 	var value int
 	saveStateCmd := &cobra.Command{
@@ -96,15 +90,6 @@ func main() {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			privKey, err := ioutil.ReadFile(privFile)
-			if err != nil {
-				return errors.Wrap(err, "private key file not found")
-			}
-
-			privKey, err = base64.StdEncoding.DecodeString(string(privKey))
-			if err != nil {
-				log.Fatalf("Cannot read priv file: %s", privFile)
-			}
 
 			// define data
 			msgData := MessageData{Value: value}
@@ -119,21 +104,15 @@ func main() {
 				Data:    data,
 			}
 
-			signer := auth.NewEd25519Signer(privKey)
-			contract, err := newContract(chainID, writeURI, readURI, contractName)
+			err = cli.CallContract(defaultContract, "SaveState", msg, nil)
 			if err != nil {
 				return err
 			}
 
-			_, err = contract.Call("SaveState", msg, signer, nil)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("set value %d for user '%s' successfully!\n", value, user)
+			log.Printf("set value %d for user '%s' successfully!\n", value, user)
 			return nil
 		},
 	}
-	saveStateCmd.Flags().StringVarP(&privFile, "key", "k", "key", "private key file")
 	saveStateCmd.Flags().IntVarP(&value, "value", "v", 0, "integer value")
 	saveStateCmd.Flags().StringVarP(&user, "user", "u", "loom", "user")
 
@@ -148,47 +127,28 @@ func main() {
 				Owner: user,
 			}
 
-			contract, err := newContract(chainID, writeURI, readURI, contractName)
-			if err != nil {
+			if err := cli.StaticCallContract(defaultContract, "GetState", params, &result); err != nil {
 				return err
 			}
 
-			if _, err := contract.StaticCall("GetState", params, &result); err != nil {
-				return err
-			}
 			var msgData MessageData
-			err = json.Unmarshal(result.State, &msgData)
+			err := json.Unmarshal(result.State, &msgData)
 			if err != nil {
 				return err
 			}
-			fmt.Printf("get value %d from user '%s'\n", msgData.Value, user)
+			log.Printf("get value %d from user '%s'\n", msgData.Value, user)
 			return nil
 		},
 	}
-
-	getStateCmd.Flags().StringVarP(&privFile, "key", "k", "key", "private key file")
 	getStateCmd.Flags().StringVarP(&user, "user", "u", "loom", "user")
 
-	rootCmd.AddCommand(keygenCmd)
-	rootCmd.AddCommand(createAccCmd)
-	rootCmd.AddCommand(saveStateCmd)
-	rootCmd.AddCommand(getStateCmd)
+	callCmd.AddCommand(createAccCmd)
+	callCmd.AddCommand(saveStateCmd)
+	callCmd.AddCommand(getStateCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
-}
 
-func newContract(chainID, writeURI, readURI, contractName string) (*client.Contract, error) {
-	// create rpc client
-	rpcClient := client.NewDAppChainRPCClient(chainID, writeURI, readURI)
-	// resolve address
-	contractAddr, err := rpcClient.Resolve(contractName)
-	if err != nil {
-		return nil, err
-	}
-	// create contract
-	contract := client.NewContract(rpcClient, contractAddr.Local)
-	return contract, nil
 }
